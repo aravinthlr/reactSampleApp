15cf8378d0a4ee51a8c8c3bc9c469bbf27f8437b
aravinthlr <aravinthlr55@gmail.com>
2018-07-10 19:14:46 +0530

    updating with react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { Component} from "react";
+import React from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
+import {Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.css';
 
-class App extends Component{
+class InputBox extends React.Component {
+    render() {
+        return <input type={this.props.type} id={this.props.id}/>
+    }
+}
+
+class App extends React.Component {
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! test</h1>
+        <InputBox type="text" id='UserName'/><br/>
+        <InputBox type="password" id='Password'/><br/>
+        <Button bsStyle="primary">SUBMIT</Button>
       </div>
     );
   }
 }
-console.log('123',module);
+
 export default hot(module)(App);
